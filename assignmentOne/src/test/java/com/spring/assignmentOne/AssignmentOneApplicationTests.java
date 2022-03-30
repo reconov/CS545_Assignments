a73e90850daba5090d1d56fecc2324c8963d214f
Abraham Bisrat <abrahammehari0@gmail.com>
2022-03-30 00:21:49 -0500

    CS545 Assignments

---
+++ assignmentOne/src/test/java/com/spring/assignmentOne/AssignmentOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.assignmentOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
