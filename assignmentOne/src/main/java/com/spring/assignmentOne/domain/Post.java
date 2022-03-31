87d5b3134afe67b4e8923ce19ffcc9a6c55194d6
Abraham Bisrat <abrahammehari0@gmail.com>
2022-03-31 04:20:49 -0500

    Changed post and User to Entities

--- assignmentOne/src/main/java/com/spring/assignmentOne/domain/Post.java
+++ assignmentOne/src/main/java/com/spring/assignmentOne/domain/Post.java
@@ -2,12 +2,20 @@ package com.spring.assignmentOne.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class Post {
 
-    private int id;
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY )
+    @Column( nullable = false )
+    private Long id;
     private String title;
     private String content;
     private String author;
--- assignmentOne/src/main/java/com/spring/assignmentOne/domain/User.java
+++ assignmentOne/src/main/java/com/spring/assignmentOne/domain/User.java
@@ -1,13 +1,25 @@
 package com.spring.assignmentOne.domain;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
+import javax.persistence.*;
 import java.util.List;
 
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
 public class User {
 
-    long id;
-    String name;
-    List<Post> posts;
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY )
+    @Column( nullable = false)
+    private Long id;
+    private String name;
+
+//    @OneToMany
+//    private List<Post> posts;
+
 }
