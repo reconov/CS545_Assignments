c1b3e55bdb273283a4c8b12035d4b4dfd285cbff
Abraham Bisrat <abrahammehari0@gmail.com>
2022-03-30 21:44:58 -0500

    Added update post

--- assignmentOne/src/main/java/com/spring/assignmentOne/controller/PostController.java
+++ assignmentOne/src/main/java/com/spring/assignmentOne/controller/PostController.java
@@ -3,6 +3,7 @@ package com.spring.assignmentOne.controller;
 import com.spring.assignmentOne.domain.Post;
 import com.spring.assignmentOne.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,15 +21,22 @@ public class PostController {
         return postService.getById(id);
     }
 
+    @ResponseStatus(HttpStatus.OK)
     @GetMapping("/")
     public List<Post> allPosts(){
         System.out.println("All posts was excuted");
         return postService.getAll();
     }
 
+    @ResponseStatus(HttpStatus.CREATED)
     @PostMapping()
     public boolean newPost(@RequestBody Post newPost){
         return postService.save(newPost);
+    }
+
+    @PutMapping("/{id}")
+    public void updatePost(@PathVariable int id){
+        System.out.println("Update request has been made for record with id : " + id);
     }
 
     @DeleteMapping("/{id}")
