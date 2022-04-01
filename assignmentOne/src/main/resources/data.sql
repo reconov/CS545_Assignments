d940f2eb3e68f8fde6d1bc642c38eddb2d4420af
Abraham Bisrat <abrahammehari0@gmail.com>
2022-04-01 06:45:39 -0500

    SQL test data for comments

--- assignmentOne/src/main/resources/data.sql
+++ assignmentOne/src/main/resources/data.sql
@@ -47,3 +47,84 @@ VALUES (9,'Some wise title goes here', 'Enticing title',2);
 INSERT INTO post (id, content, title, user_id)
 VALUES (10,'Some wise title goes here', 'Enticing title',2);
 
+
+-- Comments
+
+INSERT INTO comment (id, name, post_id)
+VALUES (1, 'Insightful post', 1);
+
+INSERT INTO comment (id, name, post_id)
+VALUES (2, 'Most comments on the internet are mean', 2);
+
+INSERT INTO comment (id, name, post_id)
+VALUES (3, 'Good read', 3);
+
+INSERT INTO comment (id, name, post_id)
+VALUES (4, 'Interesting points raised', 4);
+
+INSERT INTO comment (id, name, post_id)
+VALUES (5, 'Interesting points raised', 5);
+
+INSERT INTO comment (id, name, post_id)
+VALUES (6, 'Interesting points raised', 6);
+
+INSERT INTO comment (id, name, post_id)
+VALUES (7, 'Interesting points raised', 7);
+
+INSERT INTO comment (id, name, post_id)
+VALUES (8, 'Interesting points raised', 8);
+
+INSERT INTO comment (id, name, post_id)
+VALUES (9, 'Interesting points raised', 9);
+
+INSERT INTO comment (id, name, post_id)
+VALUES (10, 'Interesting points raised', 10);
+
+INSERT INTO comment (id, name, post_id)
+VALUES (11, 'Insightful post', 1);
+
+INSERT INTO comment (id, name, post_id)
+VALUES (12, 'Most comments on the internet are mean', 2);
+
+INSERT INTO comment (id, name, post_id)
+VALUES (13, 'Good read', 3);
+
+INSERT INTO comment (id, name, post_id)
+VALUES (14, 'Interesting points raised', 1);
+
+INSERT INTO comment (id, name, post_id)
+VALUES (15, 'Interesting points raised', 1);
+
+INSERT INTO comment (id, name, post_id)
+VALUES (16, 'Interesting points raised', 1);
+
+INSERT INTO comment (id, name, post_id)
+VALUES (17, 'Interesting points raised', 7);
+
+INSERT INTO comment (id, name, post_id)
+VALUES (18, 'Interesting points raised', 8);
+
+INSERT INTO comment (id, name, post_id)
+VALUES (19, 'Interesting points raised', 9);
+
+INSERT INTO comment (id, name, post_id)
+VALUES (20, 'Interesting points raised', 10);
+
+INSERT INTO comment (id, name, post_id)
+VALUES (21, 'Interesting points raised', 5);
+
+INSERT INTO comment (id, name, post_id)
+VALUES (22, 'Interesting points raised', 6);
+
+INSERT INTO comment (id, name, post_id)
+VALUES (23, 'Interesting points raised', 7);
+
+INSERT INTO comment (id, name, post_id)
+VALUES (24, 'Interesting points raised', 8);
+
+INSERT INTO comment (id, name, post_id)
+VALUES (25, 'Interesting points raised', 2);
+
+INSERT INTO comment (id, name, post_id)
+VALUES (26, 'Interesting points raised', 2);
+
