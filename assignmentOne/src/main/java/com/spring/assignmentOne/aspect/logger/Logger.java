d9f70eebe3bbb7f09fe4cbe4e293a93b0ca67e25
Abraham Bisrat <abrahammehari0@gmail.com>
2022-04-02 05:18:02 -0500

    Logger Entity

---
+++ assignmentOne/src/main/java/com/spring/assignmentOne/aspect/logger/Logger.java
@@ -0,0 +1,29 @@
+package com.spring.assignmentOne.aspect.logger;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.time.LocalDate;
+import java.time.LocalTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Logger {
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY )
+    private String transaction_id;
+
+    private LocalDate date;
+    private LocalTime time;
+    private String principle;
+    private String operation;
+
+}
