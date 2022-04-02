5966c661df50aae44d0c07ed50825c8015d02dd0
Abraham Bisrat <abrahammehari0@gmail.com>
2022-04-02 02:06:38 -0500

    findCommentByUserAndPostIdCommentId converted to direct sql query

--- assignmentOne/src/main/java/com/spring/assignmentOne/repo/CommentRepo.java
+++ assignmentOne/src/main/java/com/spring/assignmentOne/repo/CommentRepo.java
@@ -1,6 +1,7 @@
 package com.spring.assignmentOne.repo;
 
 import com.spring.assignmentOne.domain.Comment;
+import com.spring.assignmentOne.domain.Users;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
@@ -15,7 +16,12 @@ public interface CommentRepo extends CrudRepository<Comment, Long> {
 
     Optional<Comment> findById(Long id);
 
-//    List<Users>
+    @Query(value = "SELECT * from comment c" +
+            "RIGHT JOIN post p on p.id = c.post_id" +
+            "RIGHT JOIN users u on u.id = p.user_id" +
+            "WHERE u.id = :uId and p.id = :pId and c.id = :cId",
+            nativeQuery = true)
+    Comment findCommentByUserAndPostIdCommentId(Long uId, Long pId, Long cId);
 
     // query to fetch users that have more than n comments to a post
 
