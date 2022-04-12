3856ac5e3668c977141b8fa2da8ce5ac24f11f82
Abraham Bisrat <abrahammehari0@gmail.com>
2022-04-12 14:23:05 -0500

    Added security config

--- assignmentOne/src/main/java/com/spring/assignmentOne/security/config/securityConfig.java
+++ assignmentOne/src/main/java/com/spring/assignmentOne/security/config/securityConfig.java
@@ -1,6 +1,7 @@
 package com.spring.assignmentOne.security.config;
 
 import com.spring.assignmentOne.config.JwtAuthenticationEntryPoint;
+import com.spring.assignmentOne.config.JwtRequestFilter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -13,10 +14,12 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -29,24 +32,39 @@ public class securityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     private UserDetailsService jwtUserDetailsService;
 
+    @Autowired
+    private JwtRequestFilter jwtRequestFilter;
 
     @Autowired
-    private UserDetailsService userDetailsService;
-
-    @Override
-    public void configure(AuthenticationManagerBuilder auth)
+    public void configureGlobal(AuthenticationManagerBuilder auth)
             throws Exception {
-        auth.userDetailsService(userDetailsService);
+        // configure AuthenticationManager so that it knows from where to load
+        // user for the matching credentials
+        auth.userDetailsService(jwtUserDetailsService); // TODO Add pass encoder here
+    }
+
+    @Bean
+    @Override
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
                 .authorizeHttpRequests()
+                .antMatchers("/auth").permitAll()
                 .antMatchers("/admin").hasAuthority("ADMIN")
                 .antMatchers("/users").hasAnyAuthority("ADMIN", "CLIENT")
-                .antMatchers("/auth").permitAll()
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
+                // make sure we use stateless session; session won't be used to
+                // store user's state.
+                .and().exceptionHandling()
+                .authenticationEntryPoint(jwtAuthenticationEntryPoint)
+                .and().sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        // Add a filter to validate the tokes with every request
+        http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
@@ -59,11 +77,4 @@ public class securityConfig extends WebSecurityConfigurerAdapter {
 //    public PasswordEncoder passwordEncoder() {
 //        return new BCryptPasswordEncoder();
 //    }
-
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
-
 }
